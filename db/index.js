cbd83fc236eac66bd3faf0688b105cc53b090cc2
Chewie <quentin.rennies@gmail.com>
2026-03-18 15:54:10 +0100

    test

--- db/index.js
+++ db/index.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const env = process.env.NODE_ENV || "development";
-
 console.log({ env });
 
 const config = require(`../environment/${env}.js`);
 console.log({ config });
 
+const uri = process.env.MONGO_URI || config.dbUri;
+console.log({ uri });
+
 mongoose
-  .connect(config.dbUri)
+  .connect(uri)
   .then(() => {
     console.log("db connected");
   })
