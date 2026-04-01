2809e79fca42240b2b2ac66b9cf2675e7da6aa57
Chewie <quentin.rennies@gmail.com>
2026-04-01 12:59:56 +0200

    maj

--- environment/production.js
+++ environment/production.js
@@ -1,5 +1,13 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+const env = process.env;
+
+console.log(env);
+
 module.exports = {
-  dbUri:
-    "mongodb+srv://quentinrennies_db_user:kguTLGEdeMDwj3M3@cluster0.1rxmxow.mongodb.net/rdv?retryWrites=true&w=majority",
+  stripeWebhookKey: env.STRIPE_WEBHOOK_KEY_SERVER,
+  stripeSecretKey: env.STRIPE_SECRET_KEY_SERVER,
+  stripePricePlanPro: env.STRIPE_PRICE_KEY_SERVER,
+  dbUri: env.MONGO_URI_SERVER,
   port: 8080,
 };
