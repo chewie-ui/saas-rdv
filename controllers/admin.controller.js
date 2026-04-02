04e131c979f3b9be5aa620713a24f0f5cd5bbec5
Mac <mac@MacBook-Air-de-Mac.local>
2026-04-02 23:08:45 +0200

    deploy

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,10 +1,16 @@
-const env = require(`../environment/${process.env.NODE_ENV}`);
 const Stripe = require("stripe");
+let stripe;
+if (process.env.NODE_ENV === "prodution") {
+  stripe = new Stripe(process.env.STRIPE_SECRET_KEY_SERVER)
+} else {
+  const env = require(`../environment/${process.env.NODE_ENV}`);
+  stripe = new Stripe(env.stripeSecretKey);
+}
+
 
 console.log(`ENV: ${JSON.stringify(env)}`);
 console.log(`ENV avec la key: ${env.stripeSecretKey}`);
 
-const stripe = new Stripe(env.stripeSecretKey);
 
 const {
   getAppointments,
