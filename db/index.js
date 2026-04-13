781628193eca4ea7ec925fd3ce66461167824665
Chewie <quentin.rennies@gmail.com>
2026-04-13 14:44:26 +0200

    test

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const env = require(`../environment/${process.env.NODE_ENV || "development"}`);
-
+console.log("MONGO_URI =", process.env.MONGO_URI);
+console.log("MONGO_URI_SERVER =", process.env.MONGO_URI_SERVER);
 mongoose
   .connect(env.dbUri)
   .then(() => {
