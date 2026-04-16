9c2320d98ad7cd7fd4010cc943cd77c69aead6c8
Chewie <quentin.rennies@gmail.com>
2026-04-16 11:43:56 +0200

    debug on

--- scripts/clean-companies.js
+++ scripts/clean-companies.js
@@ -4,28 +4,23 @@ const Companies = require("../db/models/company/company.model");
 const User = require("../db/models/user.model");
 
 async function clean() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(
+    "mongodb+srv://quentinrennies_db_user:kguTLGEdeMDwj3M3@cluster0.1rxmxow.mongodb.net/rdv?retryWrites=true&w=majority",
+  );
 
-    console.log("✅ Connecté à Mongo");
+  const companies = await Companies.find({}).populate("owner");
 
-    const companies = await Companies.find({}).populate("owner");
+  const orphans = companies.filter((c) => !c.owner);
 
-    const orphans = companies.filter((c) => !c.owner);
+  console.log("A supprimer:", orphans.length);
 
-    console.log("🗑️ A supprimer:", orphans.length);
-
-    for (const c of orphans) {
-      console.log("DELETE:", c._id.toString());
-      await Companies.findByIdAndDelete(c._id);
-    }
-
-    console.log("✅ Terminé");
-    process.exit(0);
-  } catch (err) {
-    console.error("❌ Erreur:", err);
-    process.exit(1);
+  for (const c of orphans) {
+    console.log("DELETE:", c._id.toString());
+    await Companies.findByIdAndDelete(c._id);
   }
+
+  console.log("Terminé");
+  process.exit();
 }
 
 clean();
