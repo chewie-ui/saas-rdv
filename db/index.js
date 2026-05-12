2e423c233911f817249feba88fd1a6488d2509e8
Jean <quentin.rennies@gmail.com>
2026-05-12 12:05:20 +0200

    de

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const env = require(`../environment/${process.env.NODE_ENV || "development"}`);
+console.log(process.env.NODE_ENV);
 
 mongoose
   .connect(env.dbUri)
